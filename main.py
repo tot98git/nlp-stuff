deb50b2c29ea13d75f43b995878c457b026ca221
work <work@w.local>
2020-04-11 22:15:54 +0200

    Integrations

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from classifier import Clasifier
+from classifier import Classifier
+from word2vec import WordEmbedding
 
 def main():
-    nb = Clasifier()
+    nb = Classifier()
+    w2v = WordEmbedding()
+
     nb.main()
+    w2v.main()
 
 if __name__=="__main__": 
     main() 
